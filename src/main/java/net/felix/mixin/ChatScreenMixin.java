c8245a3f75ee8aaec39d082254c60cb448391c2f
Nico <nico.rigsinger@hotmail.com>
2025-08-26 23:30:39 +0200

    EmojiIconButtonWidget hinzugefügt.

--- src/main/java/net/felix/mixin/ChatScreenMixin.java
+++ src/main/java/net/felix/mixin/ChatScreenMixin.java
@@ -3,12 +3,16 @@ package net.felix.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.felix.CCLiveUtilities;
 import net.felix.accessor.ScreenAccess;
 import net.felix.interfaces.SymbolInsertable;
+import net.felix.widget.EmojiIconButtonWidget;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,12 +22,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-/*
-    Ändere die ChatScreen größe, damit der Button reinpasst
-*/
-
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin extends Screen implements SymbolInsertable {
+
+    @Unique private static final Identifier TEXTURE = Identifier.of(CCLiveUtilities.MOD_ID, "emoji_button");
 
     @Shadow
     protected TextFieldWidget chatField;
@@ -31,13 +33,33 @@ public class ChatScreenMixin extends Screen implements SymbolInsertable {
     @Unique
     private static final int TEXT_BOX_CURSOR_WIDTH = 8;
 
+    @Unique
+    private EmojiIconButtonWidget emojiIconButtonWidget;
+
     protected ChatScreenMixin(Text title) {
         super(title);
+    }
+
+    public void addSymbolChatComponents() {
+        int padding = 2;
+
+        int emojiButtonX = this.width - padding - 12;
+        int emojiButtonY = this.height - padding - 12;
+
+        emojiIconButtonWidget = new EmojiIconButtonWidget(12, 12, ScreenTexts.EMPTY, 12, 12, TEXTURE, button -> {
+            button.setOutlined(!button.isOutlined());
+        }, null);
+
+        emojiIconButtonWidget.setPosition(emojiButtonX, emojiButtonY);
+        this.addDrawableChild(emojiIconButtonWidget);
+
+
     }
 
     @Inject(method = "init", at = @At(value = "RETURN"))
     private void onInit(CallbackInfo ci) {
         //((ScreenAccess) this).addSymbolChatComponents();
+        addSymbolChatComponents();
     }
 
     @Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;mouseClicked(DDI)Z"), cancellable = true)
@@ -51,6 +73,10 @@ public class ChatScreenMixin extends Screen implements SymbolInsertable {
     public boolean skipArrowKeys(ChatScreen instance, int keyCode, int scanCode, int modifiers, Operation<Boolean> original) {
         return ((keyCode != GLFW.GLFW_KEY_UP && keyCode != GLFW.GLFW_KEY_DOWN) || !this.chatField.isFocused()) && original.call(this, keyCode, scanCode, modifiers);
     }
+
+    /*
+        Ändere die ChatScreen größe, damit der Button reinpasst
+    */
 
     @ModifyConstant(method = "init",constant = @Constant(intValue = 4, ordinal = 1),require = 1)
     private int changeTextBoxWidth(int original) {
